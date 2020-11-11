c73edf0edd404c59eed019d54b9642aa91c3231a
thomas.joubert <thomas.joubert@epita.fr>
2020-11-11 16:18:20 +0100

    [ADD] custom exception header to obstacle

--- src/obstacle.cc
+++ src/obstacle.cc
@@ -1,11 +1,12 @@
 #include "obstacle.hh"
+#include "exceptions/unvalidpath.hh"
 
 Obstacle::Obstacle(int pos_x,
             int pos_y,
             int len_x,
             int len_y,
             std::string path_to_texture)
-    : Object(pos_x, pos_y, len_x, len_y),
+    : Object(pos_x, pos_y, len_x, len_y)
 {
     sf::Texture texture;
     if (!texture.loadFromFile(path_to_texture))
