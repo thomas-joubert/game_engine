afe5724946a4245a52b91ac7a07c9e5bd7bd5d6f
thomas.joubert <thomas.joubert@epita.fr>
2020-11-11 16:05:59 +0100

    [ADD] Obstacle class

---
+++ src/obstacle.cc
@@ -0,0 +1,20 @@
+#include "obstacle.hh"
+
+Obstacle::Obstacle(int pos_x,
+            int pos_y,
+            int len_x,
+            int len_y,
+            std::string path_to_texture)
+    : Object(pos_x, pos_y, len_x, len_y),
+{
+    sf::Texture texture;
+    if (!texture.loadFromFile(path_to_texture))
+        throw UnvalidPath(path_to_texture);
+
+    sprite_ = sf::Sprite(texture);
+}
+
+sf::Sprite Obstacle::sprite_get(void)
+{
+    return sprite_;
+}
---
+++ src/obstacle.hh
@@ -0,0 +1,23 @@
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+
+#include <string>
+
+#include "object.hh"
+
+/* Represents an obstacle, unmovable object that has a particular sprite */
+
+class Obstacle : public Object
+{
+    public :
+    Obstacle(int pos_x,
+            int pos_y,
+            int len_x,
+            int len_y,
+            std::string path_to_texture);
+
+    sf::Sprite sprite_get(void);
+
+    private :
+        sf::Sprite sprite_;
+};
