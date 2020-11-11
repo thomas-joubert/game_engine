53cab04b2f918042b5fa028cc7367c509a7d18a0
thomas.joubert <thomas.joubert@epita.fr>
2020-11-11 17:16:32 +0100

    [ADD] simple main.cc

---
+++ src/main.cc
@@ -0,0 +1,37 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+
+#include "object.hh"
+#include "obstacle.hh"
+
+int main(void)
+{
+    // Main window instanciation
+    sf::RenderWindow renderWindow(sf::VideoMode(1000, 1000, sf::Style::Default), "Something");
+    renderWindow.setVerticalSyncEnabled(true);
+
+    auto obstacle = Obstacle(0, 0, 100, 100, "/home/thomas/game_engine/tes1.jpg");
+
+    while (renderWindow.isOpen())
+    {
+        // Stores the event (keyboards...)
+        sf::Event event;
+
+        //pollEvent renvoie true si un event est en attente, false sinon
+        while (renderWindow.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed
+                    || (event.type == sf::Event::KeyPressed
+                        && event.key.code == sf::Keyboard::Tab))
+                renderWindow.close();
+
+            renderWindow.clear();
+            renderWindow.draw(obstacle);
+            renderWindow.display();
+        }
+    }
+
+    return 0;
+}
