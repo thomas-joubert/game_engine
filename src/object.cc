d5a4b8236b5c3c073fc598ee70a2e7ddb7c3f9ae
thomas.joubert <thomas.joubert@epita.fr>
2020-11-14 12:44:52 +0100

    [FIX] add exception when coordinates passed to constructor are negative

--- src/object.cc
+++ src/object.cc
@@ -1,3 +1,6 @@
+#include <exception>
+#include <stdexcept>
+
 #include "object.hh"
 
 Object::Object(int pos_x,
@@ -8,7 +11,10 @@ Object::Object(int pos_x,
     pos_y_(pos_y),
     len_x_(len_x),
     len_y_(len_y)
-{}
+{
+    if (pos_x < 0 || pos_y < 0 || len_x < 0 || len_y < 0)
+        throw std::invalid_argument("An object cannot be initialize with negative values");
+}
 
 
 int Object::pos_x_get(void)
