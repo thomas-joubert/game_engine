113164914fa2a5a7934e67ea4917893f5a54d10a
thomas.joubert <thomas.joubert@epita.fr>
2020-11-22 19:10:18 +0100

    feat: add collisionHandler class to cmake and to main loop

--- src/main.cc
+++ src/main.cc
@@ -6,19 +6,25 @@
 #include "object.hh"
 #include "obstacle.hh"
 #include "player.hh"
+#include "collision_handler.hh"
 
 #define ACCELERATION 10
 
-bool movement(sf::Event event, player::Player &player)
+bool movement(sf::Event event, player::Player &player, level::CollisionHandler collisionHandler)
 {
-    if (event.key.code == sf::Keyboard::Up)
-        player.impulse(sf::Vector2f(0, -ACCELERATION));
-    else if (event.key.code == sf::Keyboard::Down)
-        player.impulse(sf::Vector2f(0, ACCELERATION));
-    else if (event.key.code == sf::Keyboard::Left)
-        player.impulse(sf::Vector2f(-ACCELERATION, 0));
-    else if (event.key.code == sf::Keyboard::Right)
-        player.impulse(sf::Vector2f(ACCELERATION, 0));
+    auto up = sf::Vector2f(0, -ACCELERATION);
+    auto down = sf::Vector2f(0, ACCELERATION);
+    auto left = sf::Vector2f(-ACCELERATION, 0);
+    auto right = sf::Vector2f(ACCELERATION, 0);
+
+    if (event.key.code == sf::Keyboard::Up && !collisionHandler.check(up, player))
+        player.impulse(up);
+    else if (event.key.code == sf::Keyboard::Down && !collisionHandler.check(down, player))
+        player.impulse(down);
+    else if (event.key.code == sf::Keyboard::Left && !collisionHandler.check(left, player))
+        player.impulse(left);
+    else if (event.key.code == sf::Keyboard::Right && !collisionHandler.check(right, player))
+        player.impulse(right);
     else
         return false;
 
@@ -36,6 +42,8 @@ int main(void)
             100, 100, 100, 100);
     auto player = player::Player("/home/thomas/game_engine/test2.png",
             0, 0, 100, 100);
+    auto collisionHandler = level::CollisionHandler(renderWindow.getSize());
+    collisionHandler.add_obstacle(&obstacle);
 
     while (renderWindow.isOpen())
     {
@@ -50,7 +58,7 @@ int main(void)
                         && event.key.code == sf::Keyboard::Tab))
                 renderWindow.close();
             else if (event.type == sf::Event::KeyPressed)
-                movement(event, player);
+                movement(event, player, collisionHandler);
 
             player.move();
             renderWindow.clear();
